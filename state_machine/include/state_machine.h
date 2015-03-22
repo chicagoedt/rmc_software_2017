f2f2722464d3968be7c3331884d6899fe1dac861
L0g1x-Krystian <krgebis@gmail.com>
2015-03-22 14:43:13 -0500

    Now can read from goalpoints.yaml file and rearrange the goals that we want to go to

--- state_machine/include/state_machine.h
+++ state_machine/include/state_machine.h
@@ -6,25 +6,52 @@
 #include <actionlib/client/simple_action_client.h>
 #include <geometry_msgs/Pose.h>
 
+#include <vector>
+#include <sstream>
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 
 class StateMachineBase
 {
+
+	const int MAX_GOAL_POINTS; // Refer to Goal Points vector
+
 	public:
 
 		StateMachineBase(void);
 		~StateMachineBase(void);
 
 		void run();
+		bool Initialize();
 
 	private:
 
-		MoveBaseClient 						_moveBaseAC;
-		move_base_msgs::MoveBaseGoal 		_moveBaseGoal;
+		ros::NodeHandle					_nh;
+
+		MoveBaseClient 					_moveBaseAC;
+		move_base_msgs::MoveBaseGoal 	_moveBaseGoal;
 
 		bool startConnectionAC;
 		bool sendGoalToAC(geometry_msgs::Pose goalPose);
+
+
+		/*
+		 * @brief 
+		 *
+		 * Dumping Zone: Point 1. 		This will be our 'origin' to return to dump.
+		 * Digging Zone: Point 2/3/4. 	In our vector array we wont ittirate from Point 2
+		 *								to Point 3 to Point 4, instead these will be our 
+		 *								be each one of our dig zone end points for each 
+		 *								traversial itteration (Drive, Dig, Dump). 
+		 * Aligning Zone: Point 5		After we finish digging at one of our dig zone end 
+		 *								points (2,3, or 4), Point 5 will be the following 
+		 *								irrirated goal sent to actionlib server.
+		 * Example: 	Localize --> Point 2 --> Point 5 --> Point 0 
+		 *						 --> Point 3 --> Point 5 --> Point 0
+		 *						 --> Point 4 --> Point 5 --> Point 0
+		 */
+		std::vector<geometry_msgs::Pose> _goalPoints;
 
 };
 
--- state_machine/src/state_machine.cpp
+++ state_machine/src/state_machine.cpp
@@ -1,7 +1,8 @@
 #include "state_machine.h"
 
 StateMachineBase::StateMachineBase(void):
-	_moveBaseAC("move_base", true)
+	_moveBaseAC("move_base", true), _nh("state_machine"),
+   MAX_GOAL_POINTS(5)
 {
 
 }
@@ -11,28 +12,66 @@ StateMachineBase::~StateMachineBase(void)
 
 }
 
+bool StateMachineBase::Initialize()
+{
+  std::string   goalParamName = "GoalPoint";
+
+  for(int i = 0; i < MAX_GOAL_POINTS; i++)
+  {
+    std::ostringstream gp; gp << "GoalPoint" << i;
+
+    ROS_INFO_STREAM(gp.str());
+
+    if (_nh.hasParam(gp.str()))
+    {
+      std::vector<double>   goalXY;
+
+      geometry_msgs::Pose   tmpPose;
+
+      _nh.getParam(gp.str(), goalXY);
+
+      tmpPose.position.x = goalXY[0];
+      tmpPose.position.y = goalXY[1];
+
+      _goalPoints.push_back(tmpPose);
+    }
+    else
+    {
+      ROS_ERROR_STREAM("Please fill the goalpoints.yaml for GoalPoint" << i);
+      return false;
+    }
+  }
+  
+
+
+  return true;
+}
+
+
 void StateMachineBase::run()
 {
 	while(!_moveBaseAC.waitForServer(ros::Duration(2.0)))
 	{
-    	ROS_INFO("Waiting for the move_base action server to come up");
+    ROS_INFO("Waiting for the move_base action server to come up");
 	}
 
 	ROS_INFO("Established Connection with move_base ActionServer.");
 
 	_moveBaseGoal.target_pose.header.frame_id = "odom";
-  	_moveBaseGoal.target_pose.header.stamp = ros::Time::now();
+  _moveBaseGoal.target_pose.header.stamp = ros::Time::now();
 
-  	_moveBaseGoal.target_pose.pose.position.x = 1.0;
-  	_moveBaseGoal.target_pose.pose.orientation.w = 1.0;
+  //_moveBaseGoal.target_pose.pose.position.x = 7.0;
+  //_moveBaseGoal.target_pose.pose.orientation.w = 1.0;
 
-  	ROS_INFO("Sending goal");
-  	_moveBaseAC.sendGoal(_moveBaseGoal);
+  //_moveBaseGoal.target_pose = _goalPoints.at(0);
 
-  	_moveBaseAC.waitForResult();
+  ROS_INFO("Sending goal");
+  _moveBaseAC.sendGoal(_moveBaseGoal);
 
-  	if(_moveBaseAC.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    	ROS_INFO("Hooray, the base moved 1 meter forward");
-  	else
-    	ROS_INFO("The base failed to move forward 1 meter for some reason");
+  _moveBaseAC.waitForResult();
+
+  if(_moveBaseAC.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  ROS_INFO("Hooray, the base moved 1 meter forward");
+  else
+  ROS_INFO("The base failed to move forward 1 meter for some reason");
 }

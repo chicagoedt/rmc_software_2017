3faaffe9452ce40a60436e489a0dbfdef9a64c49
Basheer Subei <basheersubei@gmail.com>
2015-04-03 02:14:32 -0500

    teensy subscribes to ARUCO transform and publishes yaw error_angle.

--- teensy_servo/src/teensy_node/teensy_node.ino
+++ teensy_servo/src/teensy_node/teensy_node.ino
@@ -5,7 +5,8 @@
 #include <ros.h>
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
-# include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Quaternion.h>
+#include <std_msgs/Float32.h>
 #include <ros/time.h>
 #include <Servo.h> 
 
@@ -14,34 +15,41 @@ Servo myservo;  // create servo object to control a servo
 int pos;    // variable to store the servo position 
 
 
-//void messageCb( const std_msgs::Empty& toggle_msg){
-//  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-//}
 
-//ros::Subscriber<std_msgs::Empty> sub("toggle_led", messageCb );
+float error_angle = 0;  // in degrees
+std_msgs::Float32 angle_message;
+ros::Publisher servo_pub("error_angle", &angle_message);
 
+void transform_callback( const geometry_msgs::TransformStamped& t){
+  error_angle = atan(t.transform.translation.x / t.transform.translation.z) * 180 / 3.14159;  // in degrees
+  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
+}
+
+ros::Subscriber<geometry_msgs::TransformStamped> transform_sub("/ar_single_board/transform", transform_callback );
 
 
 geometry_msgs::TransformStamped t;
 tf::TransformBroadcaster broadcaster;
 geometry_msgs::Quaternion q;
 
+
 char parent[] = "/servo_mount";
 char child[] = "/camera";
 
 void setup()
 {
-//  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT);
   myservo.attach(9);        // attaches the servo on pin 9 to the servo object 
   nh.initNode();            // initializes ROS node
+  nh.advertise(servo_pub);
+  nh.subscribe(transform_sub);
   broadcaster.init(nh);     // initializes tf broadcaster
-//  nh.subscribe(sub);
     pos = 0;
     t.header.frame_id = parent;
     t.child_frame_id = child;
 }
 
-void publish_spin_pause()
+void broadcast_spin_pause()
 {    
     double angle = map(pos, 0, 180, -90, 90);
 
@@ -52,6 +60,11 @@ void publish_spin_pause()
     t.transform.rotation = q;
     t.header.stamp = nh.now();
     broadcaster.sendTransform(t);
+    
+    // FOR DEBUGGING ONLY
+    angle_message.data = error_angle;
+    servo_pub.publish(&angle_message);
+    
     nh.spinOnce();
     delay(10);
 }
@@ -62,12 +75,12 @@ void loop()
   for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    publish_spin_pause();
+    broadcast_spin_pause();
   } 
   for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
   {                                
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    publish_spin_pause();  
+    broadcast_spin_pause();  
   } 
 
 }
